afcd46d25d004105ff19ceff192bb94c7c0f0a10
prajwalthapaa <121283639+prajwalthapaa@users.noreply.github.com>
2025-03-02 16:46:08 +0545

    Create db.js

---
+++ backend/configs/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Database connected!");
+  } catch (error) {
+    console.log("Data base connection error!");
+    console.log(error.message);
+    process.exit(1);
+  }
+};
