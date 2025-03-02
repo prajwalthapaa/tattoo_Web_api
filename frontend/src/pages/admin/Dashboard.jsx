bc427d6ab22ccc8fe9244021d831bd2692b8cf1d
prajwalthapaa <121283639+prajwalthapaa@users.noreply.github.com>
2025-03-02 16:33:05 +0545

    Create Dashboard.jsx

---
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div className='w-full h-screen'>
+    <img src="/dashboard.avif" alt="" />
+    </div>
+  )
+}
+
+export default Dashboard
